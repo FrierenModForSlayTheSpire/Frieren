5cfcb7092c824af3c3d40f7160f9f4c725f19bf5
Arkalin <1346912913@qq.com>
2024-01-24 17:36:30 +0800

    perf:card_number

--- src/main/java/FrierenMod/cards/white/FangYuMoFa.java
+++ src/main/java/FrierenMod/cards/white/FangYuMoFa.java
@@ -21,7 +21,7 @@ public class FangYuMoFa extends CustomCard{
     private static final CardStrings CARD_STRINGS = CardCrawlGame.languagePack.getCardStrings(ID); // 从游戏系统读取本地化资源
     private static final String NAME = CARD_STRINGS.NAME; // 读取本地化的名字
     private static final String IMG_PATH = "FrierenModResources/img/cards/Strike.png";
-    private static final int COST = 1;
+    private static final int COST = 0;
     private static final String DESCRIPTION = CARD_STRINGS.DESCRIPTION; // 读取本地化的描述
     private static final CardType TYPE = CardType.SKILL;
     private static final CardColor COLOR = FRIEREN_CARD;
