e3b6bbcce8fa16813bdb06b7feadab4142e1995f
figure <666>
2024-01-27 13:00:26 +0800

    feat:防御变零费

--- src/main/java/FrierenMod/cards/white/Defend.java
+++ src/main/java/FrierenMod/cards/white/Defend.java
@@ -15,7 +15,7 @@ public class Defend extends CustomCard{
     private static final CardStrings CARD_STRINGS = CardCrawlGame.languagePack.getCardStrings(ID);
     private static final String NAME = CARD_STRINGS.NAME;
     private static final String IMG_PATH = "FrierenModResources/img/cards/Strike.png";
-    private static final int COST = 1;
+    private static final int COST = 0;
     private static final String DESCRIPTION = CARD_STRINGS.DESCRIPTION;
     private static final CardType TYPE = CardType.SKILL;
     private static final CardColor COLOR = FRIEREN_CARD;
