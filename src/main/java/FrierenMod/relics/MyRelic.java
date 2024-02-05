fb27f95fd53b59a4834a4fbde349f4bf6d320a28
Figure <a13621935008@163.com>
2024-02-05 16:52:31 +0800

    feat:修改了圣杖之证的贴图

--- src/main/java/FrierenMod/relics/MyRelic.java
+++ src/main/java/FrierenMod/relics/MyRelic.java
@@ -13,7 +13,7 @@ import com.megacrit.cardcrawl.rooms.AbstractRoom;
 public class MyRelic extends CustomRelic {
     public static final String ID = ModHelper.makePath("MyRelic");
     // 图片路径
-    private static final String IMG_PATH = "FrierenModResources/img/relics/MyRelic.png";
+    private static final String IMG_PATH = "FrierenModResources/img/relics/HolyEmblem.png";
     // 遗物类型
     private static final RelicTier RELIC_TIER = RelicTier.STARTER;
     // 点击音效
