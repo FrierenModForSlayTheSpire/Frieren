51e5b42a33cdf3473e1368d76460aa0790080694
Figure <a13621935008@163.com>
2024-02-05 19:27:14 +0800

    feat:增加了欺骗魔族的小把戏和隐藏魔力的卡图

--- src/main/java/FrierenMod/cards/tempCards/HideMagic.java
+++ src/main/java/FrierenMod/cards/tempCards/HideMagic.java
@@ -18,7 +18,7 @@ public class HideMagic extends CustomCard{
     public static final String ID = ModHelper.makePath(HideMagic.class.getSimpleName());
     private static final CardStrings CARD_STRINGS = CardCrawlGame.languagePack.getCardStrings(ID); // 从游戏系统读取本地化资源
     private static final String NAME = CARD_STRINGS.NAME; // 读取本地化的名字
-    private static final String IMG_PATH = "FrierenModResources/img/cards/Strike.png";
+    private static final String IMG_PATH = "FrierenModResources/img/cards/HideMagic_skill.png";
     private static final int COST = 0;
     private static final String DESCRIPTION = CARD_STRINGS.DESCRIPTION; // 读取本地化的描述
     private static final CardType TYPE = CardType.SKILL;
