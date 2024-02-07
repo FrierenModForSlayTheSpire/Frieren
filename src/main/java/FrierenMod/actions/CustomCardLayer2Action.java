b16213873c67834841b1911a8a66867e3919ebf6
Arkalin <1346912913@qq.com>
2024-02-07 15:10:46 +0800

    fix：赛莉耶的魔导书bug

--- src/main/java/FrierenMod/actions/CustomCardLayer2Action.java
+++ src/main/java/FrierenMod/actions/CustomCardLayer2Action.java
@@ -1,14 +1,11 @@
 package FrierenMod.actions;
 
 import FrierenMod.cards.optionCards.zeerie.layer2.*;
+import FrierenMod.cards.optionCards.zeerie.layer4.ChantOption;
 import com.megacrit.cardcrawl.actions.AbstractGameAction;
 import com.megacrit.cardcrawl.cards.AbstractCard;
 
 public class CustomCardLayer2Action extends CustomCardLayerAction {
-    private static final int DRAW0 = 1;
-    private static final int DRAW1 = 2;
-    private static final int DRAW2 = 3;
-    private static final int DRAW3 = 5;
     private static final int MAGIC0 = 2;
     private static final int MAGIC1 = 4;
     private static final int MAGIC2 = 6;
@@ -20,6 +17,9 @@ public class CustomCardLayer2Action extends CustomCardLayerAction {
     private static final int SCRY0 = 2;
     private static final int SCRY1 = 3;
     private static final int SCRY2 = 4;
+    private static final int CHANT0 = 2;
+    private static final int CHANT1 = 3;
+    private static final int CHANT2 = 4;
     private static final int CARD2 = 1;
     private static final int CARD3 = 2;
     private static final int RETAIN2 = 1;
@@ -40,19 +40,19 @@ public class CustomCardLayer2Action extends CustomCardLayerAction {
         super.initLayer(cost);
         switch (cost){
             case 0:
-                layer.add(new DrawOption(currentLegendMagic,DRAW0));
+                layer.add(new ChantOption(currentLegendMagic,CHANT0));
                 layer.add(new MagicPowerOption(currentLegendMagic,MAGIC0));
                 layer.add(new MagicPowerInHandOption(currentLegendMagic,MAGIC_IN_HAND0));
                 layer.add(new ScryOption(currentLegendMagic,SCRY0));
                 break;
             case 1:
-                layer.add(new DrawOption(currentLegendMagic,DRAW1));
+                layer.add(new ChantOption(currentLegendMagic,CHANT1));
                 layer.add(new MagicPowerOption(currentLegendMagic,MAGIC1));
                 layer.add(new MagicPowerInHandOption(currentLegendMagic,MAGIC_IN_HAND1));
                 layer.add(new ScryOption(currentLegendMagic,SCRY1));
                 break;
             case 2:
-                layer.add(new DrawOption(currentLegendMagic,DRAW2));
+                layer.add(new ChantOption(currentLegendMagic,CHANT2));
                 layer.add(new MagicPowerOption(currentLegendMagic,MAGIC2));
                 layer.add(new GainRandomCardOption(currentLegendMagic,CARD2));
                 layer.add(new SelfRetainOption(currentLegendMagic,RETAIN2));
@@ -61,7 +61,6 @@ public class CustomCardLayer2Action extends CustomCardLayerAction {
                 layer.add(new CostZeroOption(currentLegendMagic,COST_ZERO2));
                 break;
             case 3:
-                layer.add(new DrawOption(currentLegendMagic,DRAW3));
                 layer.add(new MagicPowerOption(currentLegendMagic,MAGIC3));
                 layer.add(new GainRandomCardOption(currentLegendMagic,CARD3));
                 layer.add(new SelfRetainOption(currentLegendMagic,RETAIN3));
--- src/main/java/FrierenMod/actions/CustomCardLayer4Action.java
+++ src/main/java/FrierenMod/actions/CustomCardLayer4Action.java
@@ -1,13 +1,15 @@
 package FrierenMod.actions;
 
+import FrierenMod.cards.optionCards.zeerie.layer2.DrawOption;
 import FrierenMod.cards.optionCards.zeerie.layer4.*;
 import com.megacrit.cardcrawl.actions.AbstractGameAction;
 import com.megacrit.cardcrawl.cards.AbstractCard;
 
 public class CustomCardLayer4Action extends CustomCardLayerAction {
-    private static final int CHANT0 = 2;
-    private static final int CHANT1 = 3;
-    private static final int CHANT2 = 4;
+    private static final int DRAW0 = 1;
+    private static final int DRAW1 = 2;
+    private static final int DRAW2 = 3;
+    private static final int DRAW3 = 5;
     private static final int DEXTERITY0 = 2;
     private static final int DEXTERITY1 = 3;
     private static final int DEXTERITY2 = 4;
@@ -27,23 +29,24 @@ public class CustomCardLayer4Action extends CustomCardLayerAction {
         super.initLayer(cost);
         switch (cost){
             case 0:
-                layer.add(new ChantOption(currentLegendMagic,CHANT0));
+                layer.add(new DrawOption(currentLegendMagic,DRAW0));
                 layer.add(new DexterityOption(currentLegendMagic,DEXTERITY0));
                 layer.add(new VulnerableOption(currentLegendMagic,VULNERABLE0));
                 break;
             case 1:
-                layer.add(new ChantOption(currentLegendMagic,CHANT1));
+                layer.add(new DrawOption(currentLegendMagic,DRAW1));
                 layer.add(new DexterityOption(currentLegendMagic,DEXTERITY1));
                 layer.add(new VulnerableOption(currentLegendMagic,VULNERABLE1));
                 break;
             case 2:
-                layer.add(new ChantOption(currentLegendMagic,CHANT2));
+                layer.add(new DrawOption(currentLegendMagic,DRAW2));
                 layer.add(new DexterityOption(currentLegendMagic,DEXTERITY2));
                 layer.add(new VulnerableOption(currentLegendMagic,VULNERABLE2));
                 layer.add(new IntangibleOption(currentLegendMagic,INTANGIBLE2));
                 layer.add(new UpgradeOption(currentLegendMagic));
                 break;
             case 3:
+                layer.add(new DrawOption(currentLegendMagic,DRAW3));
                 layer.add(new IntangibleOption(currentLegendMagic,INTANGIBLE3));
                 layer.add(new StrengthOption(currentLegendMagic));
                 layer.add(new RemoveDebuffOption(currentLegendMagic));
