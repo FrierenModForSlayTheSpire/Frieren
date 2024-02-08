feae1591a308643d78d85ff06a28d3f7a0ebcb8c
figure <163>
2024-02-09 02:16:42 +0800

    feat:增加精准咏唱卡图，优化kiss代码

--- src/main/java/FrierenMod/cards/white/PreciseChant.java
+++ src/main/java/FrierenMod/cards/white/PreciseChant.java
@@ -15,7 +15,7 @@ public class PreciseChant extends AbstractFrierenCard {
     public static final String ID = ModInfo.makeID(PreciseChant.class.getSimpleName());
     private static final CardStrings CARD_STRINGS = CardCrawlGame.languagePack.getCardStrings(ID);
     private static final String NAME = CARD_STRINGS.NAME;
-    private static final String IMG_PATH = "FrierenModResources/img/cards/Strike.png";
+    private static final String IMG_PATH = "FrierenModResources/img/cards/PreciseChant_skill.png";
     private static final int COST = 1;
     private static final String DESCRIPTION = CARD_STRINGS.DESCRIPTION;
     private static final CardType TYPE = CardType.SKILL;
