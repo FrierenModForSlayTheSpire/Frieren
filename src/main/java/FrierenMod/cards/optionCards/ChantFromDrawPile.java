d11a82e4c6b05211a1f91f15a8615b01ece1768a
figure <163>
2024-02-08 20:16:25 +0800

    feat：咏唱的三张选项新增卡图，改为skill

--- src/main/java/FrierenMod/cards/optionCards/ChantFromDrawPile.java
+++ src/main/java/FrierenMod/cards/optionCards/ChantFromDrawPile.java
@@ -13,10 +13,10 @@ public class ChantFromDrawPile extends AbstractFrierenCard {
     public static final String ID = ModInfo.makeID(ChantFromDrawPile.class.getSimpleName());
     private static final CardStrings CARD_STRINGS = CardCrawlGame.languagePack.getCardStrings(ID); // 从游戏系统读取本地化资源
     private static final String NAME = CARD_STRINGS.NAME; // 读取本地化的名字
-    private static final String IMG_PATH = "FrierenModResources/img/cards/Strike.png";
+    private static final String IMG_PATH = "FrierenModResources/img/cards/DrawPile_skill.png";
     private static final int COST = -2;
     private static final String DESCRIPTION = CARD_STRINGS.DESCRIPTION; // 读取本地化的描述
-    private static final CardType TYPE = CardType.POWER;
+    private static final CardType TYPE = CardType.SKILL;
     private static final CardColor COLOR = CardColor.COLORLESS;
     private static final CardRarity RARITY = CardRarity.SPECIAL;
     private static final CardTarget TARGET = CardTarget.NONE;
--- src/main/java/FrierenMod/cards/optionCards/ChantFromHand.java
+++ src/main/java/FrierenMod/cards/optionCards/ChantFromHand.java
@@ -13,10 +13,10 @@ public class ChantFromHand extends AbstractFrierenCard {
     public static final String ID = ModInfo.makeID(ChantFromHand.class.getSimpleName());
     private static final CardStrings CARD_STRINGS = CardCrawlGame.languagePack.getCardStrings(ID); // 从游戏系统读取本地化资源
     private static final String NAME = CARD_STRINGS.NAME; // 读取本地化的名字
-    private static final String IMG_PATH = "FrierenModResources/img/cards/Strike.png";
+    private static final String IMG_PATH = "FrierenModResources/img/cards/Hand_skill.png";
     private static final int COST = -2;
     private static final String DESCRIPTION = CARD_STRINGS.DESCRIPTION; // 读取本地化的描述
-    private static final CardType TYPE = CardType.POWER;
+    private static final CardType TYPE = CardType.SKILL;
     private static final CardColor COLOR = CardColor.COLORLESS;
     private static final CardRarity RARITY = CardRarity.SPECIAL;
     private static final CardTarget TARGET = CardTarget.NONE;
