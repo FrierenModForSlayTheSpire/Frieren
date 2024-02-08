4c3d5f3f694b86e8bbe24868ee9c0a939cafef38
figure <163>
2024-02-09 01:16:10 +0800

    feat：增加两种魔力卡图，破灭之雷魔法卡图

--- src/main/java/FrierenMod/cards/tempCards/MagicPower.java
+++ src/main/java/FrierenMod/cards/tempCards/MagicPower.java
@@ -12,7 +12,7 @@ public class MagicPower extends AbstractFrierenCard {
     public static final String ID = ModInfo.makeID(MagicPower.class.getSimpleName());
     private static final CardStrings CARD_STRINGS = CardCrawlGame.languagePack.getCardStrings(ID); // 从游戏系统读取本地化资源
     private static final String NAME = CARD_STRINGS.NAME; // 读取本地化的名字
-    private static final String IMG_PATH = "FrierenModResources/img/cards/Strike.png";
+    private static final String IMG_PATH = "FrierenModResources/img/cards/MagicPower_skill.png";
     private static final int COST = 0;
     private static final String DESCRIPTION = CARD_STRINGS.DESCRIPTION; // 读取本地化的描述
     private static final CardType TYPE = CardType.SKILL;
