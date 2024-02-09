4c905eb56e6f7a7b7ca23c8b28467bcac0eca3f4
figure <163>
2024-02-09 18:55:11 +0800

    feat: 增加魔法想象力，魔法包裹 魔法探知，打开时空之门 真面目卡图

--- src/main/java/FrierenMod/cards/white/MagicDetect.java
+++ src/main/java/FrierenMod/cards/white/MagicDetect.java
@@ -17,10 +17,10 @@ public class MagicDetect extends AbstractFrierenCard {
     public static final String ID = ModInfo.makeID(MagicDetect.class.getSimpleName());
     private static final CardStrings CARD_STRINGS = CardCrawlGame.languagePack.getCardStrings(ID);
     private static final String NAME = CARD_STRINGS.NAME;
-    private static final String IMG_PATH = "FrierenModResources/img/cards/Strike.png";
+    private static final String IMG_PATH = "FrierenModResources/img/cards/MagicDetect_attack.png";
     private static final int COST = 1;
     private static final String DESCRIPTION = CARD_STRINGS.DESCRIPTION;
-    private static final CardType TYPE = CardType.SKILL;
+    private static final CardType TYPE = CardType.ATTACK;
     private static final CardColor COLOR = FRIEREN_CARD;
     private static final CardRarity RARITY = CardRarity.UNCOMMON;
     private static final CardTarget TARGET = CardTarget.ENEMY;
--- src/main/java/FrierenMod/cards/white/TimeDoor.java
+++ src/main/java/FrierenMod/cards/white/TimeDoor.java
@@ -21,7 +21,7 @@ public class TimeDoor extends AbstractFrierenCard {
     public static final String ID = ModInfo.makeID(TimeDoor.class.getSimpleName());
     private static final CardStrings CARD_STRINGS = CardCrawlGame.languagePack.getCardStrings(ID);
     private static final String NAME = CARD_STRINGS.NAME;
-    private static final String IMG_PATH = "FrierenModResources/img/cards/Strike.png";
+    private static final String IMG_PATH = "FrierenModResources/img/cards/TimeDoor_skill.png";
     private static final int COST = -2;
     private static final String DESCRIPTION = CARD_STRINGS.DESCRIPTION;
     private static final CardType TYPE = CardType.SKILL;
