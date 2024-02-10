ee8cd4193ae3a147dda8bafb5ead64bda62a5a89
figure <163>
2024-02-10 16:45:16 +0800

    feat: 增加葡萄魔法卡图

--- src/main/java/FrierenMod/cards/white/GrapeMagic.java
+++ src/main/java/FrierenMod/cards/white/GrapeMagic.java
@@ -16,7 +16,7 @@ public class GrapeMagic extends AbstractFrierenCard {
     public static final String ID = ModInfo.makeID(GrapeMagic.class.getSimpleName());
     private static final CardStrings CARD_STRINGS = CardCrawlGame.languagePack.getCardStrings(ID);
     private static final String NAME = CARD_STRINGS.NAME;
-    private static final String IMG_PATH = "FrierenModResources/img/cards/Strike_attack.png";
+    private static final String IMG_PATH = "FrierenModResources/img/cards/GrapeMagic_skill.png";
     private static final int COST = 1;
     private static final String DESCRIPTION = CARD_STRINGS.DESCRIPTION;
     private static final CardType TYPE = CardType.SKILL;
