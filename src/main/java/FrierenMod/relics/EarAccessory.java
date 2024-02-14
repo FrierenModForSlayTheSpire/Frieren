af44c65351ddc4e6f9432c5372f0385e603b9c13
figure <1>
2024-02-14 23:34:00 +0800

    feat：增加遗物 精美的耳饰

---
+++ src/main/java/FrierenMod/relics/EarAccessory.java
@@ -0,0 +1,32 @@
+package FrierenMod.relics;
+
+import FrierenMod.actions.MakeMagicPowerInHandAction;
+import FrierenMod.cards.tempCards.MagicPower;
+import FrierenMod.utils.ModInformation;
+import com.megacrit.cardcrawl.actions.common.MakeTempCardInDrawPileAction;
+import com.megacrit.cardcrawl.actions.common.MakeTempCardInHandAction;
+import com.megacrit.cardcrawl.actions.common.RelicAboveCreatureAction;
+import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
+import com.megacrit.cardcrawl.relics.AbstractRelic;
+
+public class EarAccessory extends AbstractFrierenRelic {
+    public static final String ID = ModInformation.makeID(EarAccessory.class.getSimpleName());
+    public EarAccessory() {
+        super(ID, RelicTier.COMMON);
+    }
+
+    public String getUpdatedDescription() {
+        return this.DESCRIPTIONS[0];
+    }
+
+    public AbstractRelic makeCopy() {
+        return new EarAccessory();
+    }
+    @Override
+    public void atBattleStart() {
+        flash();
+        this.addToBot(new RelicAboveCreatureAction(AbstractDungeon.player, this));
+        this.addToBot(new MakeMagicPowerInHandAction(2,true));
+    }
+}
+
