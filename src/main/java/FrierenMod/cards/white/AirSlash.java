71baf5ca58552aaf4da4a4abc6331dca028d0cd3
figure <1>
2024-02-17 14:51:58 +0800

    feat：新增攻击卡空气斩，增加新的6张攻击卡的卡图

---
+++ src/main/java/FrierenMod/cards/white/AirSlash.java
@@ -0,0 +1,31 @@
+package FrierenMod.cards.white;
+
+import FrierenMod.cards.AbstractFrierenCard;
+import FrierenMod.powers.SpeedFlowPower;
+import FrierenMod.utils.ModInformation;
+import com.megacrit.cardcrawl.actions.common.ApplyPowerAction;
+import com.megacrit.cardcrawl.actions.common.DamageAction;
+import com.megacrit.cardcrawl.cards.DamageInfo;
+import com.megacrit.cardcrawl.actions.AbstractGameAction;
+import com.megacrit.cardcrawl.characters.AbstractPlayer;
+import com.megacrit.cardcrawl.monsters.AbstractMonster;
+
+public class AirSlash extends AbstractFrierenCard {
+    public static final String ID = ModInformation.makeID(AirSlash.class.getSimpleName());
+    public AirSlash() {
+        super(ID, 2, CardType.ATTACK, CardRarity.UNCOMMON, CardTarget.ENEMY);
+        this.damage = this.baseDamage = 16;
+    }
+    @Override
+    public void upgrade() {
+        if (!this.upgraded) {
+            this.upgradeName();
+            this.upgradeDamage(4);
+        }
+    }
+    @Override
+    public void use(AbstractPlayer p, AbstractMonster m) {
+        this.addToBot(new DamageAction(m, new DamageInfo(p, this.damage, this.damageTypeForTurn), AbstractGameAction.AttackEffect.SLASH_DIAGONAL));
+        this.addToBot(new ApplyPowerAction(p,p,new SpeedFlowPower(p)));
+    }
+}
