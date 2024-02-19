9f90a2b524b24dd96e56ecd1726e1217c8520ba7
Arkalin <1346912913@qq.com>
2024-02-20 00:28:43 +0800

    feat:新卡loop again

---
+++ src/main/java/FrierenMod/cards/white/LoopAgain.java
@@ -0,0 +1,26 @@
+package FrierenMod.cards.white;
+
+import FrierenMod.actions.LoopAgainAction;
+import FrierenMod.cards.AbstractFrierenCard;
+import FrierenMod.utils.ModInformation;
+import com.megacrit.cardcrawl.characters.AbstractPlayer;
+import com.megacrit.cardcrawl.monsters.AbstractMonster;
+
+public class LoopAgain extends AbstractFrierenCard {
+    public static final String ID = ModInformation.makeID(LoopAgain.class.getSimpleName());
+    public LoopAgain() {
+        super(ID, 1, CardType.SKILL, CardRarity.UNCOMMON, CardTarget.NONE);
+        this.magicNumber = this.baseMagicNumber = 3;
+    }
+    @Override
+    public void upgrade() {
+        if (!this.upgraded) {
+            this.upgradeName();
+            this.upgradeMagicNumber(-1);
+        }
+    }
+    @Override
+    public void use(AbstractPlayer p, AbstractMonster m) {
+        this.addToBot(new LoopAgainAction(this.magicNumber));
+    }
+}
