88088e35137dec514eb72edc7e830282d4200ada
Arkalin <arkalin@outlook.com>
2024-03-14 17:14:43 +0800

    feat:离别馈赠效果补全

--- src/main/java/FrierenMod/cards/white/ManaCollapse.java
+++ src/main/java/FrierenMod/cards/white/ManaCollapse.java
@@ -5,8 +5,10 @@ import FrierenMod.cards.AbstractFrierenCard;
 import FrierenMod.utils.ModInformation;
 import com.megacrit.cardcrawl.actions.AbstractGameAction;
 import com.megacrit.cardcrawl.actions.common.DamageAction;
+import com.megacrit.cardcrawl.cards.AbstractCard;
 import com.megacrit.cardcrawl.cards.DamageInfo;
 import com.megacrit.cardcrawl.characters.AbstractPlayer;
+import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
 import com.megacrit.cardcrawl.monsters.AbstractMonster;
 
 public class ManaCollapse extends AbstractFrierenCard {
@@ -15,6 +17,7 @@ public class ManaCollapse extends AbstractFrierenCard {
         super(ID, 1, CardType.ATTACK, CardRarity.UNCOMMON, CardTarget.ENEMY);
         this.damage = this.baseDamage = 9;
         this.magicNumber = this.baseMagicNumber = 1;
+        this.secondMagicNumber = this.baseSecondMagicNumber = 20;
         this.shuffleBackIntoDrawPile = true;
     }
     @Override
@@ -26,7 +29,27 @@ public class ManaCollapse extends AbstractFrierenCard {
     }
     @Override
     public void use(AbstractPlayer p, AbstractMonster m) {
-        this.addToBot(new DamageAction(m, new DamageInfo(p, this.damage, this.damageTypeForTurn), AbstractGameAction.AttackEffect.SLASH_DIAGONAL));
-        this.addToBot(new MakeManaInHandAction(this.magicNumber));
+        if(AbstractDungeon.actionManager.cardsPlayedThisTurn.size() >= 10){
+            this.addToBot(new DamageAction(m, new DamageInfo(p, this.secondMagicNumber, this.damageTypeForTurn), AbstractGameAction.AttackEffect.SLASH_DIAGONAL));
+        }
+        else {
+            this.addToBot(new DamageAction(m, new DamageInfo(p, this.damage, this.damageTypeForTurn), AbstractGameAction.AttackEffect.SLASH_DIAGONAL));
+            this.addToBot(new MakeManaInHandAction(this.magicNumber));
+        }
+    }
+    public void applyPowers() {
+        super.applyPowers();
+        this.secondMagicNumber = this.baseSecondMagicNumber = this.damage + 11;
+        if (AbstractDungeon.actionManager.cardsPlayedThisTurn.size() >= 10) {
+            this.shuffleBackIntoDrawPile = false;
+        }
+        initializeDescription();
+    }
+    public void triggerOnGlowCheck() {
+        if (AbstractDungeon.actionManager.cardsPlayedThisTurn.size() >= 10) {
+            this.glowColor = AbstractCard.GOLD_BORDER_GLOW_COLOR.cpy();
+        } else {
+            this.glowColor = AbstractCard.BLUE_BORDER_GLOW_COLOR.cpy();
+        }
     }
 }
