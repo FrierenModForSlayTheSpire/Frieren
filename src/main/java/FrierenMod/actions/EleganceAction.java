f9279dfeb4cb6e317454079b9f74a9444cf1f2a4
Arkalin <arkalin@outlook.com>
2024-03-15 17:41:36 +0800

    feat:将英姿Deprecated

--- src/main/java/FrierenMod/actions/EleganceAction.java
+++ src/main/java/FrierenMod/actions/EleganceAction.java
@@ -4,14 +4,16 @@ import FrierenMod.cards.AbstractFrierenCard;
 import com.megacrit.cardcrawl.actions.AbstractGameAction;
 import com.megacrit.cardcrawl.actions.common.DrawCardAction;
 import com.megacrit.cardcrawl.actions.common.GainBlockAction;
-import com.megacrit.cardcrawl.actions.utility.WaitAction;
+import com.megacrit.cardcrawl.cards.AbstractCard;
 import com.megacrit.cardcrawl.cards.DamageInfo;
 import com.megacrit.cardcrawl.core.AbstractCreature;
-import com.megacrit.cardcrawl.core.Settings;
 import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
+import com.megacrit.cardcrawl.helpers.GetAllInBattleInstances;
 import com.megacrit.cardcrawl.monsters.AbstractMonster;
 import com.megacrit.cardcrawl.vfx.combat.FlashAtkImgEffect;
 
+import java.util.UUID;
+@Deprecated
 public class EleganceAction extends AbstractGameAction {
     private final DamageInfo info;
 
@@ -20,30 +22,59 @@ public class EleganceAction extends AbstractGameAction {
         this.info = info;
         this.c = c;
         setValues(target, info);
-        this.actionType = AbstractGameAction.ActionType.DAMAGE;
-        this.duration = Settings.ACTION_DUR_MED;
+        this.actionType = ActionType.DAMAGE;
+        this.duration = 0.1F;
     }
     public void update() {
-        if (this.duration == Settings.ACTION_DUR_MED &&
-                this.target != null) {
+        if (this.duration == 0.1F && this.target != null) {
             AbstractDungeon.effectList.add(new FlashAtkImgEffect(this.target.hb.cX, this.target.hb.cY, AbstractGameAction.AttackEffect.NONE));
             c.applyPowers();
             this.target.damage(this.info);
             this.addToBot(new GainBlockAction(AbstractDungeon.player,c.block));
             if(c.baseMagicNumber > 0)
-                this.addToBot(new DrawCardAction(c.magicNumber));
+                this.addToBot(new DrawCardAction(c.baseMagicNumber));
             if ((((AbstractMonster)this.target).isDying || this.target.currentHealth <= 0) && !this.target.halfDead &&
                     !this.target.hasPower("Minion")) {
-                this.addToBot(new IncreaseEleganceMiscAction(c.uuid,c.misc,2));
+                this.increaseEleganceMisc(c.uuid,2);
                 if(((AbstractMonster) this.target).type == AbstractMonster.EnemyType.ELITE || ((AbstractMonster) this.target).type == AbstractMonster.EnemyType.BOSS)
-                    this.addToBot(new IncreaseEleganceSecondMiscAction(c.uuid,c.secondMisc,1));
+                    this.increaseEleganceSecondMisc(c.uuid,1);
             }
             if ((AbstractDungeon.getCurrRoom()).monsters.areMonstersBasicallyDead())
                 AbstractDungeon.actionManager.clearPostCombatActions();
         }
-        tickDuration();
-        if (this.isDone) {
-            addToTop(new WaitAction(Settings.ACTION_DUR_MED));
+        this.tickDuration();
+    }
+    private void increaseEleganceMisc(UUID targetUUID, int miscIncrease){
+        for (AbstractCard c : AbstractDungeon.player.masterDeck.group) {
+            if (!c.uuid.equals(targetUUID))
+                continue;
+            c.misc += miscIncrease;
+            c.baseDamage += c.misc;
+            c.baseBlock += c.misc;
+            c.isDamageModified = false;
+            c.isBlockModified = false;
+        }
+        for (AbstractCard c : GetAllInBattleInstances.get(targetUUID)) {
+            c.misc += miscIncrease;
+            c.baseDamage += c.misc;
+            c.baseBlock += c.misc;
+        }
+    }
+    private void increaseEleganceSecondMisc(UUID targetUUID, int miscIncrease){
+        for (AbstractCard c : AbstractDungeon.player.masterDeck.group) {
+            if(c instanceof AbstractFrierenCard){
+                if (!c.uuid.equals(targetUUID))
+                    continue;
+                ((AbstractFrierenCard) c).secondMisc += miscIncrease;
+                c.baseMagicNumber = ((AbstractFrierenCard) c).secondMisc;
+                c.isMagicNumberModified = false;
+            }
+        }
+        for (AbstractCard c : GetAllInBattleInstances.get(targetUUID)) {
+            if(c instanceof AbstractFrierenCard){
+                ((AbstractFrierenCard) c).secondMisc += miscIncrease;
+                c.baseMagicNumber = ((AbstractFrierenCard) c).secondMisc;
+            }
         }
     }
 }
--- src/main/java/FrierenMod/deprecated/Elegance.java
+++ src/main/java/FrierenMod/deprecated/Elegance.java
@@ -1,15 +1,21 @@
-package FrierenMod.cards.white;
+package FrierenMod.deprecated;
 
 import FrierenMod.actions.EleganceAction;
 import FrierenMod.cards.AbstractFrierenCard;
 import FrierenMod.utils.ModInformation;
+import basemod.abstracts.CustomSavable;
+import com.google.gson.reflect.TypeToken;
 import com.megacrit.cardcrawl.cards.DamageInfo;
 import com.megacrit.cardcrawl.characters.AbstractPlayer;
 import com.megacrit.cardcrawl.core.CardCrawlGame;
 import com.megacrit.cardcrawl.monsters.AbstractMonster;
 
-public class Elegance extends AbstractFrierenCard {
+import java.lang.reflect.Type;
+
+@Deprecated
+public class Elegance extends AbstractFrierenCard implements CustomSavable<Integer> {
     public static final String ID = ModInformation.makeID(Elegance.class.getSimpleName());
+    private int secondMiscVariable;
     public Elegance() {
         super(ID, 2, CardType.ATTACK, CardRarity.UNCOMMON, CardTarget.ENEMY);
         this.misc = 8;
@@ -17,19 +23,46 @@ public class Elegance extends AbstractFrierenCard {
         this.baseDamage = this.misc;
         this.baseBlock = this.misc;
         this.baseMagicNumber = this.secondMisc;
-        this.exhaust = true;
     }
     @Override
     public void upgrade() {
         if (!this.upgraded) {
             this.upgradeName();
-            this.exhaust = false;
             this.rawDescription = CardCrawlGame.languagePack.getCardStrings(ID).UPGRADE_DESCRIPTION;
             this.initializeDescription();
         }
     }
+    public void applyPowers() {
+        this.baseDamage = this.baseBlock = this.misc;
+        this.magicNumber = this.baseMagicNumber = this.secondMisc;
+        super.applyPowers();
+        this.initializeDescription();
+    }
     @Override
     public void use(AbstractPlayer p, AbstractMonster m) {
         this.addToBot(new EleganceAction(m,new DamageInfo(p, this.damage, this.damageTypeForTurn),this));
+    }
+
+    @Override
+    public Integer onSave() {
+        return secondMiscVariable;
+    }
+
+    @Override
+    public void onLoad(Integer i) {
+        if (i != null) {
+            secondMiscVariable = i;
+        }
+    }
+    @Override
+    public Type savedType() {
+        return new TypeToken<Integer>(){}.getType();
+    }
+
+    @Override
+    public void onLoadedMisc() {
+        this.secondMisc = secondMiscVariable;
+        this.magicNumber = this.baseMagicNumber = this.secondMisc;
+        this.initializeDescription();
     }
 }
