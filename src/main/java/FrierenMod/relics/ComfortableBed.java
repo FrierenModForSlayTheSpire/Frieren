6d0d6c3f7b41dcae9449611319ba71ed5abd20ad
Arkalin <arkalin@outlook.com>
2024-03-28 16:48:33 +0800

    perf:格式化代码

--- src/main/java/FrierenMod/relics/ComfortableBed.java
+++ src/main/java/FrierenMod/relics/ComfortableBed.java
@@ -6,11 +6,14 @@ import com.megacrit.cardcrawl.actions.common.*;
 import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
 import com.megacrit.cardcrawl.monsters.AbstractMonster;
 import com.megacrit.cardcrawl.relics.AbstractRelic;
+
 public class ComfortableBed extends AbstractFrierenRelic {
     public static final String ID = ModInformation.makeID(ComfortableBed.class.getSimpleName());
+
     public ComfortableBed() {
         super(ID, RelicTier.BOSS);
     }
+
     public String getUpdatedDescription() {
         return this.DESCRIPTIONS[0];
     }
@@ -18,25 +21,27 @@ public class ComfortableBed extends AbstractFrierenRelic {
     public AbstractRelic makeCopy() {
         return new ComfortableBed();
     }
-    public boolean isNormal(){
+
+    public boolean isNormal() {
         for (AbstractMonster m : (AbstractDungeon.getMonsters()).monsters) {
             if (m.type == AbstractMonster.EnemyType.BOSS)
                 return true;
         }
         return false;
     }
+
     public void atBattleStart() {
         flash();
         this.addToBot(new RelicAboveCreatureAction(AbstractDungeon.player, this));
         this.addToBot(new MakeTempCardInDiscardAndDeckAction(new Laziness().makeCopy()));
-        }
+    }
 
-public void onEquip() {
-    AbstractDungeon.player.energy.energyMaster++;
-}
+    public void onEquip() {
+        AbstractDungeon.player.energy.energyMaster++;
+    }
 
-public void onUnequip() {
-    AbstractDungeon.player.energy.energyMaster--;
-}
+    public void onUnequip() {
+        AbstractDungeon.player.energy.energyMaster--;
+    }
 
 }
