7f66685dad03e9b8abb7271c29dd84718068b84a
Arkalin <arkalin@outlook.com>
2024-03-31 13:51:02 +0800

    perf:更名打击防御

--- src/main/java/FrierenMod/cards/white/Defend_Frieren.java
+++ src/main/java/FrierenMod/cards/white/Defend_Frieren.java
@@ -1,18 +1,25 @@
 package FrierenMod.cards.white;
 
 import FrierenMod.cards.AbstractMagicianCard;
+import FrierenMod.enums.CardEnums;
 import FrierenMod.utils.ModInformation;
 import com.megacrit.cardcrawl.actions.common.GainBlockAction;
 import com.megacrit.cardcrawl.characters.AbstractPlayer;
 import com.megacrit.cardcrawl.monsters.AbstractMonster;
 
-public class Defend extends AbstractMagicianCard {
-    public static final String ID = ModInformation.makeID(Defend.class.getSimpleName());
-    public Defend() {
-        super(ID, 1, CardType.SKILL, CardRarity.BASIC, CardTarget.SELF);
+public class Defend_Frieren extends AbstractMagicianCard {
+    public static final String ID = ModInformation.makeID(Defend_Frieren.class.getSimpleName());
+
+    public Defend_Frieren() {
+        super(ID, 1, CardType.SKILL, CardEnums.FRIEREN_CARD, CardRarity.BASIC, CardTarget.SELF);
+    }
+
+    @Override
+    public void initSpecifiedAttributes() {
         this.block = baseBlock = 5;
         this.tags.add(CardTags.STARTER_DEFEND);
     }
+
     @Override
     public void upgrade() {
         if (!this.upgraded) {
@@ -20,8 +27,9 @@ public class Defend extends AbstractMagicianCard {
             this.upgradeBlock(3);
         }
     }
+
     @Override
     public void use(AbstractPlayer p, AbstractMonster m) {
-            this.addToBot(new GainBlockAction(p,p,this.block));
+        this.addToBot(new GainBlockAction(p, p, this.block));
     }
 }
--- src/main/java/FrierenMod/cards/white/Strike_Frieren.java
+++ src/main/java/FrierenMod/cards/white/Strike_Frieren.java
@@ -1,6 +1,7 @@
 package FrierenMod.cards.white;
 
 import FrierenMod.cards.AbstractMagicianCard;
+import FrierenMod.enums.CardEnums;
 import FrierenMod.utils.ModInformation;
 import com.megacrit.cardcrawl.actions.common.DamageAction;
 import com.megacrit.cardcrawl.cards.DamageInfo;
@@ -8,10 +9,10 @@ import com.megacrit.cardcrawl.actions.AbstractGameAction;
 import com.megacrit.cardcrawl.characters.AbstractPlayer;
 import com.megacrit.cardcrawl.monsters.AbstractMonster;
 
-public class Strike extends AbstractMagicianCard {
-    public static final String ID = ModInformation.makeID(Strike.class.getSimpleName());
-    public Strike() {
-        super(ID, 1, CardType.ATTACK, CardRarity.BASIC, CardTarget.ENEMY);
+public class Strike_Frieren extends AbstractMagicianCard {
+    public static final String ID = ModInformation.makeID(Strike_Frieren.class.getSimpleName());
+    public Strike_Frieren() {
+        super(ID, 1, CardType.ATTACK, CardEnums.FRIEREN_CARD, CardRarity.BASIC, CardTarget.ENEMY);
         this.damage = this.baseDamage = 6;
         this.tags.add(CardTags.STARTER_STRIKE);
         this.tags.add(CardTags.STRIKE);
