6fe42ed682e9ef6a4a177069802244e45d0c192c
Arkalin <arkalin@outlook.com>
2024-03-31 21:59:48 +0800

    feat:添加打击防御（菲伦）图片文字资源

---
+++ src/main/java/FrierenMod/cards/canAutoAdd/purple/Defend_Fern.java
@@ -0,0 +1,38 @@
+package FrierenMod.cards.canAutoAdd.purple;
+
+import FrierenMod.cards.AbstractMagicianCard;
+import FrierenMod.enums.CardEnums;
+import FrierenMod.utils.CardInfo;
+import FrierenMod.utils.ModInformation;
+import com.megacrit.cardcrawl.actions.common.GainBlockAction;
+import com.megacrit.cardcrawl.characters.AbstractPlayer;
+import com.megacrit.cardcrawl.monsters.AbstractMonster;
+
+public class Defend_Fern extends AbstractMagicianCard {
+    public static final String ID = ModInformation.makeID(FrierenMod.cards.canAutoAdd.purple.Defend_Fern.class.getSimpleName());
+    public static final CardInfo info = new CardInfo(ID, 1, CardType.SKILL, CardEnums.FERN_CARD, CardRarity.BASIC, CardTarget.SELF);
+
+    public Defend_Fern() {
+        super(info);
+    }
+
+    @Override
+    public void initSpecifiedAttributes() {
+        this.block = baseBlock = 5;
+        this.tags.add(CardTags.STARTER_DEFEND);
+    }
+
+    @Override
+    public void upgrade() {
+        if (!this.upgraded) {
+            this.upgradeName();
+            this.upgradeBlock(3);
+        }
+    }
+
+    @Override
+    public void use(AbstractPlayer p, AbstractMonster m) {
+        this.addToBot(new GainBlockAction(p, p, this.block));
+    }
+}
+
