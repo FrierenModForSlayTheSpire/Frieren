3112d69eb1212418feb2a64557fc28ec639803af
Arkalin <arkalin@outlook.com>
2024-04-05 13:46:53 +0800

    perf:添加定叫他有来无回的triggerOnGlowCheck

--- src/main/java/FrierenMod/cards/white/LockTarget.java
+++ src/main/java/FrierenMod/cards/white/LockTarget.java
@@ -1,18 +1,21 @@
 package FrierenMod.cards.white;
 
-
 import FrierenMod.actions.LockTargetAction;
 import FrierenMod.cards.AbstractFrierenCard;
 import FrierenMod.utils.ModInformation;
 import com.megacrit.cardcrawl.characters.AbstractPlayer;
 import com.megacrit.cardcrawl.monsters.AbstractMonster;
 
+import static FrierenMod.gameHelpers.ChantHelper.getManaNumInDiscardPile;
+import static FrierenMod.gameHelpers.ChantHelper.getManaNumInDrawPile;
+
 public class LockTarget extends AbstractFrierenCard {
     public static final String ID = ModInformation.makeID(LockTarget.class.getSimpleName());
+
     public LockTarget() {
         super(ID, 1, CardType.ATTACK, CardRarity.COMMON, CardTarget.ENEMY);
         this.baseDamage = 8;
-        this.baseMagicNumber = this.magicNumber =1;
+        this.baseMagicNumber = this.magicNumber = 1;
     }
 
     @Override
@@ -22,8 +25,14 @@ public class LockTarget extends AbstractFrierenCard {
             this.upgradeDamage(3);
         }
     }
+
+    @Override
+    public void triggerOnGlowCheck() {
+        this.glowColor = getManaNumInDrawPile() >= getManaNumInDiscardPile() ? GOLD_BORDER_GLOW_COLOR : BLUE_BORDER_GLOW_COLOR;
+    }
+
     @Override
     public void use(AbstractPlayer p, AbstractMonster m) {
-        this.addToBot(new LockTargetAction(this.magicNumber,this.damage,this.damageTypeForTurn,m));
+        this.addToBot(new LockTargetAction(this.magicNumber, this.damage, this.damageTypeForTurn, m));
     }
 }
