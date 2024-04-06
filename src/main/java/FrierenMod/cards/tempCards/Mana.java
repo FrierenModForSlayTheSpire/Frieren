9bfafdad0f01337059f32892b4c616adfa92f85f
Arkalin <arkalin@outlook.com>
2024-04-06 10:03:25 +0800

    fix:魔力可以被升级的bug

--- src/main/java/FrierenMod/cards/tempCards/Mana.java
+++ src/main/java/FrierenMod/cards/tempCards/Mana.java
@@ -8,11 +8,18 @@ import com.megacrit.cardcrawl.monsters.AbstractMonster;
 
 public class Mana extends AbstractFrierenCard {
     public static final String ID = ModInformation.makeID(Mana.class.getSimpleName());
+
     public Mana() {
         super(ID, -2, CardType.STATUS, CardColor.COLORLESS, CardRarity.SPECIAL, CardTarget.NONE);
         this.isMana = true;
         this.exhaust = true;
     }
+
+    @Override
+    public boolean canUpgrade() {
+        return false;
+    }
+
     @Override
     public void use(AbstractPlayer p, AbstractMonster m) {
         this.addToBot(new ManaAction(1));
