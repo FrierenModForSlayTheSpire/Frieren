4e34c29a09743277c166c14137fddd79e1f35e78
Arkalin <arkalin@outlook.com>
2024-04-07 20:58:23 +0800

    perf:修改流转为攻击

--- src/main/java/FrierenMod/cards/white/Flow.java
+++ src/main/java/FrierenMod/cards/white/Flow.java
@@ -4,14 +4,18 @@ import FrierenMod.actions.FlowAction;
 import FrierenMod.cards.AbstractFrierenCard;
 import FrierenMod.cards.tempCards.Mana;
 import FrierenMod.utils.ModInformation;
+import com.megacrit.cardcrawl.actions.AbstractGameAction;
+import com.megacrit.cardcrawl.actions.common.DamageAction;
+import com.megacrit.cardcrawl.cards.DamageInfo;
 import com.megacrit.cardcrawl.characters.AbstractPlayer;
 import com.megacrit.cardcrawl.monsters.AbstractMonster;
 
 public class Flow extends AbstractFrierenCard {
     public static final String ID = ModInformation.makeID(Flow.class.getSimpleName());
     public Flow() {
-        super(ID, 0, CardRarity.COMMON);
+        super(ID, 0, CardType.ATTACK,CardRarity.COMMON);
         this.magicNumber = this.baseMagicNumber = 3;
+        this.damage = this.baseDamage = 3;
         this.cardsToPreview = new Mana();
     }
     @Override
@@ -19,10 +23,12 @@ public class Flow extends AbstractFrierenCard {
         if (!this.upgraded) {
             this.upgradeName();
             this.upgradeMagicNumber(2);
+            this.upgradeDamage(2);
         }
     }
     @Override
     public void use(AbstractPlayer p, AbstractMonster m) {
+        this.addToBot(new DamageAction(m, new DamageInfo(p, this.damage, this.damageTypeForTurn), AbstractGameAction.AttackEffect.SLASH_DIAGONAL));
         this.addToBot(new FlowAction(this.magicNumber));
     }
 }
