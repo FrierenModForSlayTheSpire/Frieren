7dc224a842e8ffdac24ec008ab907c174040c585
Arkalin <arkalin@outlook.com>
2024-04-07 22:46:49 +0800

    fix:全速前进数值翻倍未生效

--- src/main/java/FrierenMod/cards/white/FullAhead.java
+++ src/main/java/FrierenMod/cards/white/FullAhead.java
@@ -2,19 +2,16 @@ package FrierenMod.cards.white;
 
 import FrierenMod.cards.AbstractFrierenCard;
 import FrierenMod.powers.FullAheadPower;
-import FrierenMod.powers.OutpouringPower;
 import FrierenMod.utils.ModInformation;
 import com.megacrit.cardcrawl.actions.common.ApplyPowerAction;
 import com.megacrit.cardcrawl.characters.AbstractPlayer;
 import com.megacrit.cardcrawl.monsters.AbstractMonster;
-import com.megacrit.cardcrawl.powers.DexterityPower;
-import com.megacrit.cardcrawl.powers.LoseDexterityPower;
 
 public class FullAhead extends AbstractFrierenCard {
     public static final String ID = ModInformation.makeID(FullAhead.class.getSimpleName());
     public FullAhead() {
         super(ID, 1,CardType.POWER, CardRarity.COMMON);
-        this.magicNumber = this.baseMagicNumber = 1;
+        this.magicNumber = this.baseMagicNumber = 2;
     }
     @Override
     public void upgrade() {
