f0ed661956d17cda189f130eef01b315185512ee
Arkalin <arkalin@outlook.com>
2024-04-13 23:28:20 +0800

    perf:统一魔力名称

--- src/main/java/FrierenMod/actions/CustomCardLayer2Action.java
+++ src/main/java/FrierenMod/actions/CustomCardLayer2Action.java
@@ -5,14 +5,14 @@ import com.megacrit.cardcrawl.actions.AbstractGameAction;
 import com.megacrit.cardcrawl.cards.AbstractCard;
 
 public class CustomCardLayer2Action extends CustomCardLayerAction {
-    private static final int MAGIC0 = 2;
-    private static final int MAGIC1 = 4;
-    private static final int MAGIC2 = 6;
-    private static final int MAGIC3 = 8;
-    private static final int MAGIC_IN_HAND0 = 2;
-    private static final int MAGIC_IN_HAND1 = 3;
-    private static final int MAGIC_IN_HAND2 = 4;
-    private static final int MAGIC_IN_HAND3 = 5;
+    private static final int MANA0 = 2;
+    private static final int MANA1 = 4;
+    private static final int MANA2 = 6;
+    private static final int MANA3 = 8;
+    private static final int MANA_IN_HAND0 = 2;
+    private static final int MANA_IN_HAND1 = 3;
+    private static final int MANA_IN_HAND2 = 4;
+    private static final int MANA_IN_HAND3 = 5;
     private static final int SCRY0 = 2;
     private static final int SCRY1 = 3;
     private static final int SCRY2 = 4;
@@ -40,31 +40,31 @@ public class CustomCardLayer2Action extends CustomCardLayerAction {
         switch (cost) {
             case 0:
                 layer.add(new ChantOption(currentLegendMagic, CHANT0));
-                layer.add(new ManaInDiscardOption(currentLegendMagic, MAGIC0));
-                layer.add(new ManaInDrawOption(currentLegendMagic, MAGIC0));
+                layer.add(new ManaInDiscardOption(currentLegendMagic, MANA0));
+                layer.add(new ManaInDrawOption(currentLegendMagic, MANA0));
                 layer.add(new ScryOption(currentLegendMagic, SCRY0));
                 break;
             case 1:
                 layer.add(new ChantOption(currentLegendMagic, CHANT1));
-                layer.add(new ManaInDiscardOption(currentLegendMagic, MAGIC1));
-                layer.add(new ManaInHandOption(currentLegendMagic, MAGIC_IN_HAND1));
-                layer.add(new ManaInDrawOption(currentLegendMagic, MAGIC1));
+                layer.add(new ManaInDiscardOption(currentLegendMagic, MANA1));
+                layer.add(new ManaInHandOption(currentLegendMagic, MANA_IN_HAND1));
+                layer.add(new ManaInDrawOption(currentLegendMagic, MANA1));
                 layer.add(new ScryOption(currentLegendMagic, SCRY1));
                 break;
             case 2:
                 layer.add(new ChantOption(currentLegendMagic, CHANT2));
-                layer.add(new ManaInDiscardOption(currentLegendMagic, MAGIC2));
+                layer.add(new ManaInDiscardOption(currentLegendMagic, MANA2));
                 layer.add(new GainRandomCardOption(currentLegendMagic, CARD2));
                 layer.add(new SelfRetainOption(currentLegendMagic, RETAIN2));
-                layer.add(new ManaInHandOption(currentLegendMagic, MAGIC_IN_HAND2));
+                layer.add(new ManaInHandOption(currentLegendMagic, MANA_IN_HAND2));
                 layer.add(new ScryOption(currentLegendMagic, SCRY2));
                 layer.add(new CostZeroOption(currentLegendMagic, COST_ZERO2));
                 break;
             case 3:
-                layer.add(new ManaInDiscardOption(currentLegendMagic, MAGIC3));
+                layer.add(new ManaInDiscardOption(currentLegendMagic, MANA3));
                 layer.add(new GainRandomCardOption(currentLegendMagic, CARD3));
                 layer.add(new SelfRetainOption(currentLegendMagic, RETAIN3));
-                layer.add(new ManaInHandOption(currentLegendMagic, MAGIC_IN_HAND3));
+                layer.add(new ManaInHandOption(currentLegendMagic, MANA_IN_HAND3));
                 layer.add(new CostZeroOption(currentLegendMagic, COST_ZERO3));
                 break;
             default:
