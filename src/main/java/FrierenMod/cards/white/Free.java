486e26765e109dbb952005ab504b4e53312827b2
Arkalin <arkalin@outlook.com>
2024-04-16 22:51:44 +0800

    perf:重做物理超度

--- src/main/java/FrierenMod/cards/white/Free.java
+++ src/main/java/FrierenMod/cards/white/Free.java
@@ -1,4 +1,4 @@
-package FrierenMod.deprecated;
+package FrierenMod.cards.white;
 
 import FrierenMod.cards.AbstractBaseCard;
 import FrierenMod.enums.CardEnums;
@@ -14,6 +14,9 @@ import com.megacrit.cardcrawl.monsters.AbstractMonster;
 public class Free extends AbstractBaseCard {
     public static final String ID = ModInformation.makeID(Free.class.getSimpleName());
     public static final CardInfo info = new CardInfo(ID, 2, CardType.ATTACK, CardEnums.FRIEREN_CARD, CardRarity.UNCOMMON, CardTarget.ALL_ENEMY);
+    public static final int RATE_1 = 3;
+    public static final int RATE_2 = 5;
+
 
     public Free() {
         super(info);
@@ -25,16 +28,16 @@ public class Free extends AbstractBaseCard {
 
     @Override
     public void initSpecifiedAttributes() {
-        this.damage = this.baseDamage = 12;
+        this.damage = this.baseDamage = 8;
     }
 
     @Override
     public void triggerOnGlowCheck() {
-        if (CombatHelper.getAllManaNum() % 6 == 0)
+        if (CombatHelper.getAllManaNum() % (RATE_1 * RATE_2) == 0)
             this.glowColor = GREEN_BORDER_GLOW_COLOR;
-        else if (CombatHelper.getAllManaNum() % 2 == 0)
+        else if (CombatHelper.getAllManaNum() % RATE_1 == 0)
             this.glowColor = GOLD_BORDER_GLOW_COLOR;
-        else if (CombatHelper.getAllManaNum() % 3 == 0)
+        else if (CombatHelper.getAllManaNum() % RATE_2 == 0)
             this.glowColor = GOLD_BORDER_GLOW_COLOR;
         else
             this.glowColor = BLUE_BORDER_GLOW_COLOR;
@@ -42,9 +45,9 @@ public class Free extends AbstractBaseCard {
 
     public void use(AbstractPlayer p, AbstractMonster m) {
         this.addToBot(new DamageAllEnemiesAction(p, this.baseDamage, this.damageTypeForTurn, AbstractGameAction.AttackEffect.BLUNT_LIGHT));
-        if (CombatHelper.getAllManaNum() % 2 == 0)
+        if (CombatHelper.getAllManaNum() % RATE_1 == 0)
             this.addToBot(new DamageAllEnemiesAction(p, this.baseDamage, this.damageTypeForTurn, AbstractGameAction.AttackEffect.BLUNT_HEAVY));
-        if (CombatHelper.getAllManaNum() % 3 == 0)
+        if (CombatHelper.getAllManaNum() % RATE_2 == 0)
             this.addToBot(new DamageAllEnemiesAction(p, this.baseDamage, this.damageTypeForTurn, AbstractGameAction.AttackEffect.BLUNT_HEAVY));
     }
 
