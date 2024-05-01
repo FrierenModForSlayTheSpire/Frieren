5cf3a71293f9864b74761fb4b0cbab16c3610bad
Arkalin <arkalin@outlook.com>
2024-05-01 11:00:06 +0800

    fix:咏唱选项卡在卡牌预览界面显示错误

--- src/main/java/FrierenMod/cards/optionCards/ChantDrawPile.java
+++ src/main/java/FrierenMod/cards/optionCards/ChantDrawPile.java
@@ -7,28 +7,28 @@ import FrierenMod.utils.CardInfo;
 import FrierenMod.utils.ModInformation;
 import com.megacrit.cardcrawl.actions.AbstractGameAction;
 import com.megacrit.cardcrawl.characters.AbstractPlayer;
+import com.megacrit.cardcrawl.core.CardCrawlGame;
 import com.megacrit.cardcrawl.monsters.AbstractMonster;
 
 public class ChantDrawPile extends AbstractBaseCard {
     public static final String ID = ModInformation.makeID(ChantDrawPile.class.getSimpleName());
-    public static final CardInfo info = new CardInfo(ID, -2, CardType.SKILL, CardColor.COLORLESS, CardRarity.SPECIAL, CardTarget.NONE);
+    public static final CardInfo info = new CardInfo(ID, CardCrawlGame.languagePack.getCardStrings(ID).EXTENDED_DESCRIPTION[0], CardType.SKILL, CardTarget.NONE);
+    public static final CardInfo info2 = new CardInfo(ID, -2, CardType.SKILL, CardColor.COLORLESS, CardRarity.SPECIAL, CardTarget.NONE);
     public AbstractGameAction[] nextAction;
 
     public ChantDrawPile() {
         super(info);
-        this.secondMagicNumber = this.baseSecondMagicNumber = 0;
-        this.block = this.baseBlock = 0;
         this.cardsToPreview = new Mana();
     }
     public ChantDrawPile(int manaExhaust, int reward) {
-        super(info);
+        super(info2);
         this.secondMagicNumber = this.baseSecondMagicNumber = manaExhaust;
         this.block = this.baseBlock = reward;
         this.isSecondMagicNumberModified = (manaExhaust < reward);
         this.nextAction = null;
     }
     public ChantDrawPile(int manaExhaust, int reward, AbstractGameAction... nexAction) {
-        super(info);
+        super(info2);
         this.secondMagicNumber = this.baseSecondMagicNumber = manaExhaust;
         this.block = this.baseBlock = reward;
         this.isSecondMagicNumberModified = (manaExhaust < reward);
