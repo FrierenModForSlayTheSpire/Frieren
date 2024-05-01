fc56c9d09f76de0eb321dd4ff789b3139cc1583e
Arkalin <arkalin@outlook.com>
2024-05-01 13:59:35 +0800

    perf:优化复制体代码

---
+++ src/main/java/FrierenMod/patches/SpiegelPatch.java
@@ -0,0 +1,50 @@
+package FrierenMod.patches;
+
+import FrierenMod.enums.CharacterEnums;
+import FrierenMod.monsters.Spiegel_Frieren;
+import FrierenMod.utils.Config;
+import com.evacipated.cardcrawl.modthespire.lib.SpireInsertPatch;
+import com.evacipated.cardcrawl.modthespire.lib.SpirePatch;
+import com.evacipated.cardcrawl.modthespire.lib.SpireReturn;
+import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
+import com.megacrit.cardcrawl.dungeons.TheBeyond;
+import com.megacrit.cardcrawl.ui.buttons.ProceedButton;
+
+import java.util.ArrayList;
+import java.util.Collections;
+
+import static com.megacrit.cardcrawl.dungeons.AbstractDungeon.monsterRng;
+
+public class SpiegelPatch {
+    @SpirePatch(clz = ProceedButton.class, method = "goToDoubleBoss")
+    public static class PatchProceedButton {
+        @SpireInsertPatch(rloc = 1)
+        public static void Insert(ProceedButton __inst) {
+            if (Config.ENCOUNTER_SPIEGEL && AbstractDungeon.player.chosenClass == CharacterEnums.FRIEREN) {
+                AbstractDungeon.bossKey = getOriginalBoss();
+            }
+        }
+
+        private static String getOriginalBoss() {
+            ArrayList<String> bossList = new ArrayList<>();
+            bossList.add("Awakened One");
+            bossList.add("Donu and Deca");
+            Collections.shuffle(bossList, new java.util.Random(monsterRng.randomLong()));
+            return bossList.get(0);
+        }
+    }
+    @SpirePatch(clz = TheBeyond.class, method = "initializeBoss")
+    public static class TheBeyondPatch {
+        @SpireInsertPatch(rloc = 0, localvars = {"bossList"})
+        public static SpireReturn<Void> Insert(AbstractDungeon _inst, ArrayList<String> bossList){
+            if(Config.ENCOUNTER_SPIEGEL && AbstractDungeon.player.chosenClass == CharacterEnums.FRIEREN){
+                bossList.add(Spiegel_Frieren.MONSTER_ID);
+                bossList.add(Spiegel_Frieren.MONSTER_ID);
+                bossList.add(Spiegel_Frieren.MONSTER_ID);
+                return SpireReturn.Return();
+            }else {
+                return SpireReturn.Continue();
+            }
+        }
+    }
+}
