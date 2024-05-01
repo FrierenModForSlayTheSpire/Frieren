2c7ba92a4304b825cb257ad2d4de48251f1d57b0
Arkalin <arkalin@outlook.com>
2024-05-01 17:43:13 +0800

    fix:精灵的时间观念效果结束后怪物的意图不刷新

--- src/main/java/FrierenMod/actions/ReceivePlayerStatusAction.java
+++ src/main/java/FrierenMod/actions/ReceivePlayerStatusAction.java
@@ -4,14 +4,17 @@ import FrierenMod.gameHelpers.Status;
 import com.megacrit.cardcrawl.actions.AbstractGameAction;
 import com.megacrit.cardcrawl.characters.AbstractPlayer;
 import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
+import com.megacrit.cardcrawl.monsters.AbstractMonster;
 import com.megacrit.cardcrawl.potions.AbstractPotion;
 import com.megacrit.cardcrawl.ui.panels.EnergyPanel;
 
 public class ReceivePlayerStatusAction extends AbstractGameAction {
     private final Status status;
-    public ReceivePlayerStatusAction(Status status){
+
+    public ReceivePlayerStatusAction(Status status) {
         this.status = status;
     }
+
     @Override
     public void update() {
         AbstractPlayer p = AbstractDungeon.player;
@@ -23,17 +26,20 @@ public class ReceivePlayerStatusAction extends AbstractGameAction {
         p.relics.addAll(status.relics);
         p.powers.clear();
         p.powers.addAll(status.powers);
-        for(AbstractPotion potion:p.potions){
+        for (AbstractPotion potion : p.potions) {
             p.removePotion(potion);
         }
         p.maxOrbs = status.maxEnergy;
         EnergyPanel.totalCount = status.energy;
         p.reorganizeRelics();
         p.updatePowers();
-        for(AbstractPotion potion: status.potions){
+        for (AbstractPotion potion : status.potions) {
             AbstractDungeon.player.obtainPotion(potion);
         }
         AbstractDungeon.actionManager.cardsPlayedThisTurn.addAll(status.cardsPlayedThisTurn);
+        for (AbstractMonster mo : AbstractDungeon.getMonsters().monsters) {
+            mo.createIntent();
+        }
         this.isDone = true;
     }
 }
