12f8e6d05468ccc13d8feecf09c468162f268539
Arkalin <arkalin@outlook.com>
2024-05-19 22:28:56 +0800

    fix:油渍魔法逻辑

---
+++ src/main/java/FrierenMod/actions/OilSpellAction.java
@@ -0,0 +1,97 @@
+package FrierenMod.actions;
+
+import com.megacrit.cardcrawl.actions.AbstractGameAction;
+import com.megacrit.cardcrawl.actions.utility.WaitAction;
+import com.megacrit.cardcrawl.cards.DamageInfo;
+import com.megacrit.cardcrawl.characters.AbstractPlayer;
+import com.megacrit.cardcrawl.core.AbstractCreature;
+import com.megacrit.cardcrawl.core.Settings;
+import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
+import com.megacrit.cardcrawl.monsters.AbstractMonster;
+import com.megacrit.cardcrawl.powers.AbstractPower;
+import com.megacrit.cardcrawl.vfx.combat.FlashAtkImgEffect;
+
+public class OilSpellAction extends AbstractGameAction {
+    public int[] damage;
+
+    private int baseDamage;
+
+    private boolean firstFrame = true, utilizeBaseDamage = false;
+
+    public OilSpellAction(AbstractCreature source, int[] amount, DamageInfo.DamageType type, AbstractGameAction.AttackEffect effect, boolean isFast) {
+        this.source = source;
+        this.damage = amount;
+        this.actionType = AbstractGameAction.ActionType.DAMAGE;
+        this.damageType = type;
+        this.attackEffect = effect;
+        if (isFast) {
+            this.duration = Settings.ACTION_DUR_XFAST;
+        } else {
+            this.duration = Settings.ACTION_DUR_FAST;
+        }
+    }
+
+    public OilSpellAction(AbstractCreature source, int[] amount, DamageInfo.DamageType type, AbstractGameAction.AttackEffect effect) {
+        this(source, amount, type, effect, false);
+    }
+
+    public OilSpellAction(AbstractPlayer player, int baseDamage, DamageInfo.DamageType type, AbstractGameAction.AttackEffect effect) {
+        this(player, (int[]) null, type, effect, false);
+        this.baseDamage = baseDamage;
+        this.utilizeBaseDamage = true;
+    }
+
+    public void update() {
+        if (this.firstFrame) {
+            boolean playedMusic = false;
+            int temp = (AbstractDungeon.getCurrRoom()).monsters.monsters.size();
+            if (this.utilizeBaseDamage)
+                this.damage = DamageInfo.createDamageMatrix(this.baseDamage);
+            for (int i = 0; i < temp; i++) {
+                if (!(AbstractDungeon.getCurrRoom()).monsters.monsters.get(i).isDying &&
+                        (AbstractDungeon.getCurrRoom()).monsters.monsters.get(i).currentHealth > 0 &&
+                        !(AbstractDungeon.getCurrRoom()).monsters.monsters.get(i).isEscaping)
+                    if (playedMusic) {
+                        AbstractDungeon.effectList.add(new FlashAtkImgEffect(
+
+                                (AbstractDungeon.getCurrRoom()).monsters.monsters.get(i).hb.cX,
+                                (AbstractDungeon.getCurrRoom()).monsters.monsters.get(i).hb.cY, this.attackEffect, true));
+                    } else {
+                        playedMusic = true;
+                        AbstractDungeon.effectList.add(new FlashAtkImgEffect(
+
+                                (AbstractDungeon.getCurrRoom()).monsters.monsters.get(i).hb.cX,
+                                (AbstractDungeon.getCurrRoom()).monsters.monsters.get(i).hb.cY, this.attackEffect));
+                    }
+            }
+            this.firstFrame = false;
+        }
+        tickDuration();
+        if (this.isDone) {
+            for (AbstractPower p : AbstractDungeon.player.powers)
+                p.onDamageAllEnemies(this.damage);
+            int temp = (AbstractDungeon.getCurrRoom()).monsters.monsters.size();
+            boolean again = true;
+            for (int i = 0; i < temp; i++) {
+                AbstractMonster target = AbstractDungeon.getCurrRoom().monsters.monsters.get(i);
+                if (!target.isDeadOrEscaped()) {
+                    target.damage(new DamageInfo(this.source, this.damage[i], this.damageType));
+                    if (target.isDying || target.currentHealth <= 0) {
+                        again = false;
+                    }
+                }
+            }
+            if (again)
+                for (int i = 0; i < temp; i++) {
+                    AbstractMonster target = AbstractDungeon.getCurrRoom().monsters.monsters.get(i);
+                    if (!target.isDeadOrEscaped()) {
+                        target.damage(new DamageInfo(this.source, this.damage[i], this.damageType));
+                    }
+                }
+            if ((AbstractDungeon.getCurrRoom()).monsters.areMonstersBasicallyDead())
+                AbstractDungeon.actionManager.clearPostCombatActions();
+            if (!Settings.FAST_MODE)
+                addToTop(new WaitAction(0.1F));
+        }
+    }
+}
--- src/main/java/FrierenMod/cards/white/OilSpell.java
+++ src/main/java/FrierenMod/cards/white/OilSpell.java
@@ -1,15 +1,14 @@
 package FrierenMod.cards.white;
 
+import FrierenMod.actions.OilSpellAction;
 import FrierenMod.cards.AbstractBaseCard;
 import FrierenMod.enums.CardEnums;
 import FrierenMod.utils.CardInfo;
 import FrierenMod.utils.ModInformation;
 import com.megacrit.cardcrawl.actions.AbstractGameAction;
 import com.megacrit.cardcrawl.actions.animations.VFXAction;
-import com.megacrit.cardcrawl.actions.common.DamageAllEnemiesAction;
 import com.megacrit.cardcrawl.actions.utility.SFXAction;
 import com.megacrit.cardcrawl.characters.AbstractPlayer;
-import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
 import com.megacrit.cardcrawl.monsters.AbstractMonster;
 import com.megacrit.cardcrawl.vfx.combat.CleaveEffect;
 
@@ -43,18 +42,6 @@ public class OilSpell extends AbstractBaseCard {
     public void use(AbstractPlayer p, AbstractMonster m) {
         this.addToBot(new SFXAction("ATTACK_HEAVY"));
         this.addToBot(new VFXAction(p, new CleaveEffect(), 0.1F));
-        boolean again = true;
-        for (AbstractMonster mo : AbstractDungeon.getMonsters().monsters) {
-            if (!mo.isDead && !mo.halfDead) {
-                if (mo.currentHealth + mo.currentBlock - this.damage <= 0) {
-                    again = false;
-                    break;
-                }
-            }
-        }
-        this.addToBot(new DamageAllEnemiesAction(p, this.baseDamage, this.damageTypeForTurn, AbstractGameAction.AttackEffect.NONE));
-        if (again) {
-            this.addToBot(new DamageAllEnemiesAction(p, this.baseDamage, this.damageTypeForTurn, AbstractGameAction.AttackEffect.NONE));
-        }
+        this.addToBot(new OilSpellAction(p, this.baseDamage, this.damageTypeForTurn, AbstractGameAction.AttackEffect.NONE));
     }
 }
