5eec236dec7ff23d1445f5e0ca700983e08b62c3
Arkalin <arkalin@outlook.com>
2024-05-27 22:33:12 +0800

    perf:魔力壁垒重做

--- src/main/java/FrierenMod/cards/white/ManaBarricade.java
+++ src/main/java/FrierenMod/cards/white/ManaBarricade.java
@@ -1,24 +1,28 @@
 package FrierenMod.cards.white;
 
+import FrierenMod.actions.ChantAction;
 import FrierenMod.cards.AbstractBaseCard;
 import FrierenMod.enums.CardEnums;
-import FrierenMod.powers.ManaBarricadePower;
 import FrierenMod.utils.CardInfo;
 import FrierenMod.utils.ModInformation;
-import com.megacrit.cardcrawl.actions.common.ApplyPowerAction;
 import com.megacrit.cardcrawl.characters.AbstractPlayer;
 import com.megacrit.cardcrawl.monsters.AbstractMonster;
-import com.megacrit.cardcrawl.powers.AbstractPower;
 
 public class ManaBarricade extends AbstractBaseCard {
     public static final String ID = ModInformation.makeID(ManaBarricade.class.getSimpleName());
-    public static final CardInfo info = new CardInfo(ID, 1, CardType.POWER, CardEnums.FRIEREN_CARD, CardRarity.RARE);
+    public static final CardInfo info = new CardInfo(ID, 1, CardType.SKILL, CardEnums.FRIEREN_CARD, CardRarity.COMMON);
 
     public ManaBarricade() {
         super(info);
     }
 
-//    public ManaBarricade(CardColor color) {
+    @Override
+    public void initSpecifiedAttributes() {
+        this.block = this.baseBlock = 12;
+        this.chantX = this.baseChantX = 2;
+        this.isChantCard = true;
+    }
+    //    public ManaBarricade(CardColor color) {
 //        super(ID, 2, CardType.POWER, color, CardRarity.RARE);
 //    }
 
@@ -26,20 +30,12 @@ public class ManaBarricade extends AbstractBaseCard {
     public void upgrade() {
         if (!this.upgraded) {
             this.upgradeName();
-            this.upgradeBaseCost(0);
+            this.upgradeBlock(4);
         }
     }
 
     @Override
     public void use(AbstractPlayer p, AbstractMonster m) {
-        boolean powerExists = false;
-        for (AbstractPower pow : p.powers) {
-            if (pow.ID.equals("ManaBarricadePower")) {
-                powerExists = true;
-                break;
-            }
-        }
-        if (!powerExists)
-            this.addToBot(new ApplyPowerAction(p, p, new ManaBarricadePower(p)));
+        this.addToBot(new ChantAction(this.chantX, this.block));
     }
 }
