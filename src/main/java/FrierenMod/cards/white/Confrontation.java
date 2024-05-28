e96320a852e5d17dc075bd53e0b80031042d3d35
Arkalin <arkalin@outlook.com>
2024-05-28 12:51:01 +0800

    perf:对峙效果调整

--- src/main/java/FrierenMod/cards/white/Confrontation.java
+++ src/main/java/FrierenMod/cards/white/Confrontation.java
@@ -4,7 +4,6 @@ import FrierenMod.cards.AbstractBaseCard;
 import FrierenMod.enums.CardEnums;
 import FrierenMod.utils.CardInfo;
 import FrierenMod.utils.ModInformation;
-import com.megacrit.cardcrawl.actions.common.DrawCardAction;
 import com.megacrit.cardcrawl.actions.common.GainBlockAction;
 import com.megacrit.cardcrawl.actions.common.ModifyBlockAction;
 import com.megacrit.cardcrawl.cards.AbstractCard;
@@ -13,7 +12,7 @@ import com.megacrit.cardcrawl.monsters.AbstractMonster;
 
 public class Confrontation extends AbstractBaseCard {
     public static final String ID = ModInformation.makeID(Confrontation.class.getSimpleName());
-    public static final CardInfo info = new CardInfo(ID, 1, CardType.SKILL, CardEnums.FRIEREN_CARD, CardRarity.COMMON, CardTarget.NONE);
+    public static final CardInfo info = new CardInfo(ID, 0, CardType.SKILL, CardEnums.FRIEREN_CARD, CardRarity.COMMON, CardTarget.NONE);
 
     public Confrontation() {
         super(info);
@@ -39,7 +38,6 @@ public class Confrontation extends AbstractBaseCard {
     @Override
     public void use(AbstractPlayer p, AbstractMonster m) {
         this.addToBot(new GainBlockAction(p, p, this.block));
-        this.addToBot(new DrawCardAction(1));
     }
 
     @Override
