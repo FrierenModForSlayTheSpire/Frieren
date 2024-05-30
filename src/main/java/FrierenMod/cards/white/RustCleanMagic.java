29d960d341b35febf0b5a0b8621239846b307648
Arkalin <arkalin@outlook.com>
2024-05-30 22:41:00 +0800

    perf:铜像生锈魔法重做

--- src/main/java/FrierenMod/cards/white/RustCleanMagic.java
+++ src/main/java/FrierenMod/cards/white/RustCleanMagic.java
@@ -3,7 +3,6 @@ package FrierenMod.cards.white;
 import FrierenMod.actions.ChantAction;
 import FrierenMod.cards.AbstractBaseCard;
 import FrierenMod.enums.CardEnums;
-import FrierenMod.gameHelpers.CombatHelper;
 import FrierenMod.utils.CardInfo;
 import FrierenMod.utils.ModInformation;
 import com.megacrit.cardcrawl.actions.AbstractGameAction;
@@ -26,26 +25,16 @@ public class RustCleanMagic extends AbstractBaseCard {
 
     @Override
     public void initSpecifiedAttributes() {
-        this.magicNumber = this.baseMagicNumber = 10;
-        this.chantX = this.baseChantX = 1;
-        this.isChantCard = true;
+        this.isLegendarySpell = true;
+        this.exhaust = true;
     }
 
     @Override
     public void upgrade() {
         if (!this.upgraded) {
             this.upgradeName();
-            this.upgradeMagicNumber(-4);
+            this.upgradeBaseCost(0);
         }
-    }
-
-
-    @Override
-    public void triggerOnGlowCheck() {
-        if (CombatHelper.getAllManaNum() > 10)
-            this.glowColor = GOLD_BORDER_GLOW_COLOR;
-        else
-            this.glowColor = BLUE_BORDER_GLOW_COLOR;
     }
 
     @Override
@@ -53,6 +42,12 @@ public class RustCleanMagic extends AbstractBaseCard {
         this.addToBot(new ChantAction(this.chantX, new AbstractGameAction() {
             @Override
             public void update() {
+                for (AbstractPower po : m.powers) {
+                    if (po.type == AbstractPower.PowerType.BUFF) {
+                        this.addToTop(new RemoveSpecificPowerAction(m, m, po.ID));
+                        break;
+                    }
+                }
                 for (AbstractPower po : p.powers) {
                     if (po.type == AbstractPower.PowerType.DEBUFF) {
                         this.addToTop(new RemoveSpecificPowerAction(p, p, po.ID));
