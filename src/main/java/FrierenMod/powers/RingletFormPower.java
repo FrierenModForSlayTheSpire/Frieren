10b1344dfc0e15b2339587508af6567f4d0f6626
Arkalin <arkalin@outlook.com>
2024-05-31 12:57:17 +0800

    perf:卷发形态调整

--- src/main/java/FrierenMod/powers/RingletFormPower.java
+++ src/main/java/FrierenMod/powers/RingletFormPower.java
@@ -3,7 +3,6 @@ package FrierenMod.powers;
 import FrierenMod.cards.AbstractBaseCard;
 import FrierenMod.utils.ModInformation;
 import com.megacrit.cardcrawl.actions.common.GainBlockAction;
-import com.megacrit.cardcrawl.actions.utility.UseCardAction;
 import com.megacrit.cardcrawl.cards.AbstractCard;
 import com.megacrit.cardcrawl.core.AbstractCreature;
 
@@ -16,7 +15,7 @@ public class RingletFormPower extends AbstractBasePower {
     }
 
     @Override
-    public void onAfterUseCard(AbstractCard card, UseCardAction action) {
+    public void onExhaust(AbstractCard card) {
         if (card instanceof AbstractBaseCard && ((AbstractBaseCard) card).isMana) {
             this.flash();
             this.addToBot(new GainBlockAction(this.owner, this.owner, this.amount));
