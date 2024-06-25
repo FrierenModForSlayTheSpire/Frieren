4ad20ba7b62054bdb15a89edeb69fdf9f1c0bece
figure <1>
2024-06-25 17:26:20 +0800

    feat:新增无色卡 应急食物

---
+++ src/main/java/FrierenMod/cards/tempCards/EmergencyFood.java
@@ -0,0 +1,34 @@
+package FrierenMod.cards.tempCards;
+
+import FrierenMod.utils.CardInfo;
+import FrierenMod.utils.ModInformation;
+import com.megacrit.cardcrawl.actions.common.HealAction;
+import com.megacrit.cardcrawl.characters.AbstractPlayer;
+import com.megacrit.cardcrawl.monsters.AbstractMonster;
+import FrierenMod.cards.AbstractBaseCard;
+
+
+public class EmergencyFood extends AbstractBaseCard {
+    public static final String ID = ModInformation.makeID(EmergencyFood.class.getSimpleName());
+    public static final CardInfo info = new CardInfo(ID, 0, CardType.SKILL, CardColor.COLORLESS, CardRarity.UNCOMMON, CardTarget.SELF);
+
+    public EmergencyFood() {
+        super(info);
+        this.magicNumber = this.baseMagicNumber = 5;
+        this.exhaust = true;
+        this.selfRetain=true;
+    }
+
+    @Override
+    public void upgrade() {
+        if (!this.upgraded) {
+            this.upgradeName();
+            this.upgradeMagicNumber(2);
+        }
+    }
+    @Override
+    public void use(AbstractPlayer p, AbstractMonster m) {
+        this.addToBot(new HealAction(p,p,this.magicNumber));
+    }
+
+}
