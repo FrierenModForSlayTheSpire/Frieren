f7e68a8de7046176899868579bdd3fbbaaf8862d
Arkalin <arkalin@outlook.com>
2024-07-26 13:08:13 +0800

    修改魔法因子的style渲染

---
+++ src/main/java/FrierenMod/cards/optionCards/MagicProps/AbstractMagicProps.java
@@ -0,0 +1,10 @@
+package FrierenMod.cards.optionCards.MagicProps;
+
+import FrierenMod.cards.AbstractBaseCard;
+import FrierenMod.utils.CardInfo;
+
+public class AbstractMagicProps extends AbstractBaseCard {
+    public AbstractMagicProps(CardInfo info) {
+        super(info);
+    }
+}
