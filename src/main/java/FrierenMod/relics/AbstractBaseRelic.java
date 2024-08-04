da2b7651190391ad8f4f4530ae009ee794feeeb7
Arkalin <arkalin@outlook.com>
2024-08-04 15:27:31 +0800

    新增遗物测试图片

--- src/main/java/FrierenMod/relics/AbstractBaseRelic.java
+++ src/main/java/FrierenMod/relics/AbstractBaseRelic.java
@@ -1,6 +1,7 @@
 package FrierenMod.relics;
 
 import FrierenMod.utils.ModInformation;
+import FrierenMod.utils.ResourceChecker;
 import basemod.abstracts.CustomRelic;
 import com.badlogic.gdx.graphics.Texture;
 import com.megacrit.cardcrawl.helpers.ImageMaster;
@@ -9,7 +10,15 @@ public abstract class AbstractBaseRelic extends CustomRelic {
     public AbstractBaseRelic(String id, Texture texture, RelicTier tier, LandingSound sfx) {
         super(id, texture, tier, sfx);
     }
+
     public AbstractBaseRelic(String id, RelicTier tier) {
-        super(id, ImageMaster.loadImage(ModInformation.makeRelicImgPath(id.split(":")[1])), tier, LandingSound.FLAT);
+        super(id, getImgTexture(id), tier, LandingSound.FLAT);
+    }
+
+    private static Texture getImgTexture(String id) {
+        if (ResourceChecker.exist(ModInformation.makeRelicImgPath(id.split(":")[1]))) {
+            return ImageMaster.loadImage(ModInformation.makeRelicImgPath(id.split(":")[1]));
+        }
+        return ImageMaster.loadImage(ModInformation.makeRelicImgPath("Beta"));
     }
 }
