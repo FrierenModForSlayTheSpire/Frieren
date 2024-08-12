f60e9b8ca33ccafa522106db949f0f23e77f961b
Arkalin <arkalin@outlook.com>
2024-08-12 19:05:46 +0800

    解析结界的魔法重做

---
+++ src/main/java/FrierenMod/actions/SortDrawPileAction.java
@@ -0,0 +1,23 @@
+package FrierenMod.actions;
+
+import FrierenMod.cards.tempCards.Mana;
+import com.megacrit.cardcrawl.actions.AbstractGameAction;
+import com.megacrit.cardcrawl.cards.AbstractCard;
+import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
+
+import java.util.Comparator;
+
+public class SortDrawPileAction extends AbstractGameAction {
+    private static final Comparator<AbstractCard> BY_COST = Comparator.comparing((c) -> {
+        if (c instanceof Mana)
+            return -9;
+        else
+            return c.cost;
+    });
+
+    @Override
+    public void update() {
+        AbstractDungeon.player.drawPile.group.sort(BY_COST.reversed());
+        this.isDone = true;
+    }
+}
