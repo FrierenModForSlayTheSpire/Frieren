5d1738439f438cdfe4e17bb34e7e97d28d12b026
Arkalin <arkalin@outlook.com>
2024-08-14 16:18:14 +0800

    平衡性调整

--- src/main/java/FrierenMod/cards/white/Recycle.java
+++ src/main/java/FrierenMod/cards/white/Recycle.java
@@ -1,8 +1,10 @@
 package FrierenMod.cards.white;
 
-import FrierenMod.actions.RecycleAction;
+import FrierenMod.actions.MakeManaInDrawPileAction;
 import FrierenMod.cards.AbstractBaseCard;
 import FrierenMod.enums.CardEnums;
+import FrierenMod.gameHelpers.ActionHelper;
+import FrierenMod.gameHelpers.CombatHelper;
 import FrierenMod.utils.CardInfo;
 import FrierenMod.utils.ModInformation;
 import com.megacrit.cardcrawl.characters.AbstractPlayer;
@@ -10,31 +12,32 @@ import com.megacrit.cardcrawl.monsters.AbstractMonster;
 
 public class Recycle extends AbstractBaseCard {
     public static final String ID = ModInformation.makeID(Recycle.class.getSimpleName());
-    public static final CardInfo info = new CardInfo(ID, 0, CardType.SKILL, CardEnums.FRIEREN_CARD, CardRarity.COMMON, CardTarget.NONE);
+    public static final CardInfo info = new CardInfo(ID, 1, CardType.SKILL, CardEnums.FRIEREN_CARD, CardRarity.COMMON, CardTarget.NONE);
 
     public Recycle() {
         super(info);
     }
 
-//    public Recycle(CardColor color) {
-//        super(ID, 0, CardType.SKILL, color, CardRarity.COMMON, CardTarget.NONE);
-//    }
-
     @Override
     public void initSpecifiedAttributes() {
-        this.magicNumber = this.baseMagicNumber = 2;
+        this.exhaust = true;
     }
 
     @Override
     public void upgrade() {
         if (!this.upgraded) {
             this.upgradeName();
-            this.upgradeMagicNumber(-1);
+            this.upgradeBaseCost(0);
         }
     }
 
     @Override
     public void use(AbstractPlayer p, AbstractMonster m) {
-        this.addToBot(new RecycleAction(this.magicNumber));
+        ActionHelper.addToBotAbstract(() -> {
+            int discard = CombatHelper.getManaNumInDiscardPile();
+            if (discard > 0) {
+                this.addToBot(new MakeManaInDrawPileAction(discard));
+            }
+        });
     }
 }
