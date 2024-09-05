4174437df39c6512aa0061a6c3bf9ddb3b09da7b
Arkalin <arkalin@outlook.com>
2024-09-05 19:36:11 +0800

    猛芙下山 删除

--- src/main/java/FrierenMod/cards/white/FrushDown.java
+++ src/main/java/FrierenMod/cards/white/FrushDown.java
@@ -5,10 +5,12 @@ import FrierenMod.enums.CardEnums;
 import FrierenMod.powers.FrushDownPower;
 import FrierenMod.utils.CardInfo;
 import FrierenMod.utils.ModInformation;
+import basemod.AutoAdd;
 import com.megacrit.cardcrawl.actions.common.ApplyPowerAction;
 import com.megacrit.cardcrawl.characters.AbstractPlayer;
 import com.megacrit.cardcrawl.monsters.AbstractMonster;
 
+@AutoAdd.Ignore
 public class FrushDown extends AbstractBaseCard {
     public static final String ID = ModInformation.makeID(FrushDown.class.getSimpleName());
     public static final CardInfo info = new CardInfo(ID, 1, CardType.POWER, CardEnums.FRIEREN_CARD, CardRarity.UNCOMMON, CardTarget.SELF);
