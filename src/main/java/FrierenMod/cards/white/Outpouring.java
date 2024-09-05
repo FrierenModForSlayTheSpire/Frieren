5ab023b920828c0d43fa2a7e58f3d620b74f1070
Arkalin <arkalin@outlook.com>
2024-09-05 19:57:24 +0800

    倾泻 回滚

--- src/main/java/FrierenMod/cards/white/Outpouring.java
+++ src/main/java/FrierenMod/cards/white/Outpouring.java
@@ -2,23 +2,27 @@ package FrierenMod.cards.white;
 
 import FrierenMod.cards.AbstractBaseCard;
 import FrierenMod.enums.CardEnums;
-import FrierenMod.gameHelpers.CombatHelper;
+import FrierenMod.powers.OutpouringPower;
 import FrierenMod.utils.CardInfo;
 import FrierenMod.utils.ModInformation;
 import com.megacrit.cardcrawl.actions.common.ApplyPowerAction;
 import com.megacrit.cardcrawl.characters.AbstractPlayer;
 import com.megacrit.cardcrawl.core.CardCrawlGame;
-import com.megacrit.cardcrawl.localization.CardStrings;
 import com.megacrit.cardcrawl.monsters.AbstractMonster;
-import com.megacrit.cardcrawl.powers.StrengthPower;
 
 public class Outpouring extends AbstractBaseCard {
     public static final String ID = ModInformation.makeID(Outpouring.class.getSimpleName());
-    private static final CardStrings cardStrings = CardCrawlGame.languagePack.getCardStrings(ID);
     public static final CardInfo info = new CardInfo(ID, 1, CardEnums.FRIEREN_CARD, CardRarity.UNCOMMON);
 
     public Outpouring() {
         super(info);
+    }
+
+
+    @Override
+    public void initSpecifiedAttributes() {
+        this.magicNumber = this.baseMagicNumber = 1;
+        this.exhaust = true;
     }
 
     @Override
@@ -31,22 +35,8 @@ public class Outpouring extends AbstractBaseCard {
         }
     }
 
+    @Override
     public void use(AbstractPlayer p, AbstractMonster m) {
-        int times = CombatHelper.getContinualSynchroMaxTimes();
-        if(times > 0)
-            this.addToBot(new ApplyPowerAction(p, p, new StrengthPower(p, times)));
-    }
-
-    public void applyPowers() {
-        int times = CombatHelper.getContinualSynchroMaxTimes();
-        super.applyPowers();
-        this.rawDescription = cardStrings.DESCRIPTION;
-        this.rawDescription += String.format(cardStrings.EXTENDED_DESCRIPTION[0], times);
-        initializeDescription();
-    }
-
-    public void onMoveToDiscard() {
-        this.rawDescription = cardStrings.DESCRIPTION;
-        initializeDescription();
+        this.addToBot(new ApplyPowerAction(p, p, new OutpouringPower(p, this.magicNumber)));
     }
 }
