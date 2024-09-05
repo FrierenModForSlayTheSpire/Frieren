a251fee4d3361909d3bed2acb3c436f8230127fc
Arkalin <arkalin@outlook.com>
2024-09-05 19:36:22 +0800

    纸飞机 删除

--- src/main/java/FrierenMod/cards/white/PaperPlaneSpell.java
+++ src/main/java/FrierenMod/cards/white/PaperPlaneSpell.java
@@ -5,10 +5,12 @@ import FrierenMod.enums.CardEnums;
 import FrierenMod.powers.PaperPlaneSpellPower;
 import FrierenMod.utils.CardInfo;
 import FrierenMod.utils.ModInformation;
+import basemod.AutoAdd;
 import com.megacrit.cardcrawl.actions.common.ApplyPowerAction;
 import com.megacrit.cardcrawl.characters.AbstractPlayer;
 import com.megacrit.cardcrawl.monsters.AbstractMonster;
 
+@AutoAdd.Ignore
 public class PaperPlaneSpell extends AbstractBaseCard {
     public static final String ID = ModInformation.makeID(PaperPlaneSpell.class.getSimpleName());
     public static final CardInfo info = new CardInfo(ID, 2, CardType.POWER, CardEnums.FRIEREN_CARD, CardRarity.RARE, CardTarget.NONE);
