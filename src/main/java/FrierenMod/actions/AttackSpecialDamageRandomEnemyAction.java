5a12b3c35516dd9e017fd7d30b9e3107b9255272
Arkalin <arkalin@outlook.com>
2024-09-18 21:22:34 +0800

    修复终极同调伤害类型错误

--- src/main/java/FrierenMod/actions/AttackSpecialDamageRandomEnemyAction.java
+++ src/main/java/FrierenMod/actions/AttackSpecialDamageRandomEnemyAction.java
@@ -2,10 +2,10 @@ package FrierenMod.actions;
 
 import com.megacrit.cardcrawl.actions.AbstractGameAction;
 import com.megacrit.cardcrawl.actions.animations.VFXAction;
-import com.megacrit.cardcrawl.actions.common.DamageAction;
 import com.megacrit.cardcrawl.actions.utility.SFXAction;
 import com.megacrit.cardcrawl.cards.DamageInfo;
 import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
+import com.megacrit.cardcrawl.monsters.AbstractMonster;
 import com.megacrit.cardcrawl.vfx.combat.LightningEffect;
 
 public class AttackSpecialDamageRandomEnemyAction extends AbstractGameAction {
@@ -18,10 +18,16 @@ public class AttackSpecialDamageRandomEnemyAction extends AbstractGameAction {
     public void update() {
         this.target = AbstractDungeon.getMonsters().getRandomMonster(null, true, AbstractDungeon.cardRandomRng);
         if (this.target != null) {
-            this.addToTop(new DamageAction(this.target, new DamageInfo(AbstractDungeon.player, damage, DamageInfo.DamageType.THORNS), AttackEffect.NONE));
+            target.damage(new DamageInfo(this.source, createDamageMatrix(damage, (AbstractMonster) target), DamageInfo.DamageType.THORNS));
             this.addToTop(new SFXAction("ORB_LIGHTNING_EVOKE", 0.1F));
             this.addToTop(new VFXAction(new LightningEffect(this.target.hb.cX, this.target.hb.cY)));
         }
         this.isDone = true;
+    }
+
+    private static int createDamageMatrix(int baseDamage, AbstractMonster mo) {
+        DamageInfo info = new DamageInfo(AbstractDungeon.player, baseDamage);
+        info.applyEnemyPowersOnly(mo);
+        return info.output;
     }
 }
