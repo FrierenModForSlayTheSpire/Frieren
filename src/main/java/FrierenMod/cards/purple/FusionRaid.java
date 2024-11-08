b79798a2bff9926317c19e541cf6a122992c3394
Arkalin <arkalin@outlook.com>
2024-11-08 11:19:30 +0800

    修复一些错误

--- src/main/java/FrierenMod/cards/purple/FusionRaid.java
+++ src/main/java/FrierenMod/cards/purple/FusionRaid.java
@@ -9,17 +9,16 @@ import FrierenMod.utils.CardInfo;
 import FrierenMod.utils.ModInformation;
 import com.megacrit.cardcrawl.actions.AbstractGameAction;
 import com.megacrit.cardcrawl.actions.common.ApplyPowerAction;
-import com.megacrit.cardcrawl.actions.common.DamageAction;
+import com.megacrit.cardcrawl.actions.common.DamageAllEnemiesAction;
 import com.megacrit.cardcrawl.actions.common.DrawCardAction;
 import com.megacrit.cardcrawl.actions.common.GainEnergyAction;
 import com.megacrit.cardcrawl.cards.AbstractCard;
-import com.megacrit.cardcrawl.cards.DamageInfo;
 import com.megacrit.cardcrawl.characters.AbstractPlayer;
 import com.megacrit.cardcrawl.monsters.AbstractMonster;
 
 public class FusionRaid extends AbstractBaseCard {
     public static final String ID = ModInformation.makeID(FusionRaid.class.getSimpleName());
-    public static final CardInfo info = new CardInfo(ID, 2, CardType.ATTACK, CardEnums.FERN_CARD, CardRarity.UNCOMMON, CardTarget.ENEMY);
+    public static final CardInfo info = new CardInfo(ID, 2, CardType.ATTACK, CardEnums.FERN_CARD, CardRarity.UNCOMMON, CardTarget.ALL_ENEMY);
 
     public FusionRaid() {
         super(info);
@@ -29,6 +28,7 @@ public class FusionRaid extends AbstractBaseCard {
     public void initializeSpecifiedAttributes() {
         this.damage = this.baseDamage = 20;
         this.raidNumber = this.baseRaidNumber = 2;
+        this.isMultiDamage = true;
         this.tags.add(Enum.RAID);
         this.tags.add(Enum.FUSION);
     }
@@ -56,7 +56,7 @@ public class FusionRaid extends AbstractBaseCard {
             this.addToBot(new DrawCardAction(2));
             this.addToBot(new ApplyPowerAction(p, p, new DrawCardFusionPower(2)));
         });
-        boolean triggered2 = CombatHelper.triggerRaid(2, true, () -> this.addToBot(new DamageAction(m, new DamageInfo(p, this.damage, this.damageTypeForTurn), AbstractGameAction.AttackEffect.SLASH_DIAGONAL)));
+        boolean triggered2 = CombatHelper.triggerRaid(2, true, () -> this.addToBot(new DamageAllEnemiesAction(p, this.multiDamage, this.damageTypeForTurn, AbstractGameAction.AttackEffect.SLASH_DIAGONAL)));
         boolean triggered3 = CombatHelper.triggerRaid(3, () -> {
             this.addToBot(new GainEnergyAction(2));
             this.addToBot(new ApplyPowerAction(p, p, new EnergyFusionPower(2)));
