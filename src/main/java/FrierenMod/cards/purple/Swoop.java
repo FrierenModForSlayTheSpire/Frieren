15bd719ca254deb9d669fddea30ee92b1b7df09a
Arkalin <arkalin@outlook.com>
2024-11-08 11:50:12 +0800

    平衡性调整

--- src/main/java/FrierenMod/cards/purple/Swoop.java
+++ src/main/java/FrierenMod/cards/purple/Swoop.java
@@ -3,20 +3,20 @@ package FrierenMod.cards.purple;
 import FrierenMod.cards.AbstractBaseCard;
 import FrierenMod.enums.CardEnums;
 import FrierenMod.powers.FusionPower.DamageFusionPower;
-import FrierenMod.powers.FusionPower.EnergyFusionPower;
+import FrierenMod.powers.FusionPower.DrawCardFusionPower;
 import FrierenMod.utils.CardInfo;
 import FrierenMod.utils.ModInformation;
 import com.megacrit.cardcrawl.actions.AbstractGameAction;
 import com.megacrit.cardcrawl.actions.common.ApplyPowerAction;
 import com.megacrit.cardcrawl.actions.common.DamageAction;
-import com.megacrit.cardcrawl.actions.common.GainEnergyAction;
+import com.megacrit.cardcrawl.actions.common.DrawCardAction;
 import com.megacrit.cardcrawl.cards.DamageInfo;
 import com.megacrit.cardcrawl.characters.AbstractPlayer;
 import com.megacrit.cardcrawl.monsters.AbstractMonster;
 
 public class Swoop extends AbstractBaseCard {
     public static final String ID = ModInformation.makeID(Swoop.class.getSimpleName());
-    public static final CardInfo info = new CardInfo(ID, 2, CardType.ATTACK, CardEnums.FERN_CARD, CardRarity.COMMON, CardTarget.ENEMY);
+    public static final CardInfo info = new CardInfo(ID, 1, CardType.ATTACK, CardEnums.FERN_CARD, CardRarity.COMMON, CardTarget.ENEMY);
 
     public Swoop() {
         super(info);
@@ -25,6 +25,7 @@ public class Swoop extends AbstractBaseCard {
     @Override
     public void initializeSpecifiedAttributes() {
         this.damage = this.baseDamage = 6;
+        this.magicNumber = this.baseMagicNumber = 1;
         this.tags.add(Enum.FUSION);
     }
 
@@ -32,7 +33,7 @@ public class Swoop extends AbstractBaseCard {
     public void upgrade() {
         if (!this.upgraded) {
             this.upgradeName();
-            this.upgradeDamage(3);
+            this.upgradeMagicNumber(1);
         }
     }
 
@@ -40,8 +41,8 @@ public class Swoop extends AbstractBaseCard {
     public void use(AbstractPlayer p, AbstractMonster m) {
         this.addToBot(new DamageAction(m, new DamageInfo(p, this.damage, this.damageTypeForTurn), AbstractGameAction.AttackEffect.SLASH_DIAGONAL));
         this.addToBot(new ApplyPowerAction(p, p, new DamageFusionPower(this.damage)));
-        this.addToBot(new GainEnergyAction(1));
-        this.addToBot(new ApplyPowerAction(p, p, new EnergyFusionPower(1)));
+        this.addToBot(new DrawCardAction(p, this.magicNumber));
+        this.addToBot(new ApplyPowerAction(p, p, new DrawCardFusionPower(this.magicNumber)));
     }
 }
 
