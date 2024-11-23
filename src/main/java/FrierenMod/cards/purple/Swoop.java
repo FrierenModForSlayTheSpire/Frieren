cb46092deff64e7eb89017d7c913cdc7fe6eea6b
Arkalin <arkalin@outlook.com>
2024-11-23 20:24:31 +0800

    平衡性调整

--- src/main/java/FrierenMod/cards/purple/Swoop.java
+++ src/main/java/FrierenMod/cards/purple/Swoop.java
@@ -24,7 +24,7 @@ public class Swoop extends AbstractBaseCard {
 
     @Override
     public void initializeSpecifiedAttributes() {
-        this.damage = this.baseDamage = 9;
+        this.damage = this.baseDamage = 5;
         this.magicNumber = this.baseMagicNumber = 1;
         this.tags.add(Enum.FUSION);
     }
@@ -33,7 +33,7 @@ public class Swoop extends AbstractBaseCard {
     public void upgrade() {
         if (!this.upgraded) {
             this.upgradeName();
-            this.upgradeDamage(3);
+            this.upgradeDamage(2);
         }
     }
 
